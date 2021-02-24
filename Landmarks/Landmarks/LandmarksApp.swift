7eb7190217e3078f943b1e284cf4585a3d98325b
markkypanc <rphanchotong@gmail.com>
2021-02-24 23:12:47 +0700

    Initialize Landmarks

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Mark Rachapoom on 24/2/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
